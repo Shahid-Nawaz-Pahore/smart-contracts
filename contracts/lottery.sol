ee6317a13c3866d9f3e64666c8c5030da8bc9f51
Shahid-Nawaz-Pahore <shahidnawaz.edu.pk@gmail.com>
2023-10-09 23:01:59 -0700

    The "Lottery" smart contract allows users to enter a lottery by sending 1 Ether. The manager initiates the lottery and selects a random winner when there are at least 3 players. The winner receives the total Ether balance, and the player list resets. Only the manager can check the contract balance and start the lottery.

---
+++ contracts/lottery.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  >=0.7.0 <0.9.0;
+ contract Lottery{
+    address payable[] public Players;
+    address  manager;
+    address payable public Winner;
+    constructor(){
+        manager=msg.sender;
+    }
+    receive() external payable { 
+        require(msg.value==1 ether,"please send just one ether");
+        Players.push(payable(msg.sender));
+    }
+    function getBlance()public  view returns(uint){
+        require(manager==msg.sender,"you are not manager");
+        return address(this).balance;
+    }
+    function random()internal view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,Players.length)));
+    }
+    function getWinner()public {
+        require(msg.sender==manager,"You are not the manager");
+        require(Players.length>=3,"Player are less than 3");
+        uint r=random();
+        uint index=r%Players.length;
+        Winner=Players[index];
+        Winner.transfer(getBlance());
+        Players=new address payable[](0);
+    }
+    function getPlayer()public view returns(address payable[] memory){
+        return Players;
+    }
+ }
